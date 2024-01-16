5e97a57d3d967f238c5240a75649507e15c1c927
John <johnudoumoh63@gmail.com>
2024-01-16 14:01:43 +0100

    .

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,35 +1,5 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
-// const mailchimp = require("@mailchimp/mailchimp_marketing");
-
-// // sgMail.setApiKey(process.env.EMAIL_API_KEY);
-// const apiKey = process.env.MAILCHIMP_API_KEY
-// const apiServer = process.env.MAILCHIMP_API_SERVER
-// const audienceId = process.env.MAILCHIMP_AUDIENCE_ID
-
-// mailchimp.setConfig({
-//   apiKey: apiKey,
-//   server: apiServer,
-// });
-
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const { email } = req.body
-//   try {
-//     const response = await mailchimp.lists.addListMember(audienceId, {
-//     email_address: "johnudoumoh64@gmail.com",
-//     status: "subscribed",
-//     })
-//     res.json(`Successfully added contact as an audience member. The contact's id is ${
-//       response.id
-//     }.`)
-//   } catch (error) {
-//     res.status(500).json({ error: error })
-//   }
-// }
-
-
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse } from 'next/server';
-import axios from 'axios';
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 const apiKey = process.env.MAILCHIMP_API_KEY;
 const apiServer = process.env.MAILCHIMP_API_SERVER;
